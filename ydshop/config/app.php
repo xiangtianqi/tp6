fe053bf028bcb2963192f83c89a32fe64ac9df2d
xd <1021684144@qq.com>
2021-05-08 17:43:08 +0800

    创建多模块

--- ydshop/config/app.php
+++ ydshop/config/app.php
@@ -9,12 +9,13 @@ return [
     // 应用的命名空间
     'app_namespace'    => '',
     // 是否启用路由
-    'with_route'       => true,
+    'with_route'       => false,
     // 默认应用
     'default_app'      => 'index',
     // 默认时区
     'default_timezone' => 'Asia/Shanghai',
-
+    // 自动多应用模式
+    'auto_multi_app' => true,
     // 应用映射（自动多应用模式有效）
     'app_map'          => [],
     // 域名绑定（自动多应用模式有效）
