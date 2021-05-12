166a949acb1439e2bd095b6578872e9fd83f1213
xd <1021684144@qq.com>
2021-05-12 18:28:12 +0800

    验证器

--- ydshop/app/api/controller/Index.php
+++ ydshop/app/api/controller/Index.php
@@ -5,7 +5,9 @@ namespace app\api\controller;
 use app\BaseController;
 use  app\service\BannerService;
 use think\facade\View;
-
+use think\facade\Cache;
+use app\validate\User;
+use think\exception\ValidateException;
 
 class Index extends  BaseController
 {
@@ -13,8 +15,9 @@ class Index extends  BaseController
     {
         //return download(root_path('/public/static/').'111.jpg', 'my.jpg');
 
-         View::assign('name','ThinkPHP');
-        return View::fetch('index');
+         //View::assign('name','ThinkPHP');
+        //return View::fetch('index');
+        validate();
        /*event('UserLogin');
         // 返回数据
       $result = [
@@ -26,5 +29,23 @@ class Index extends  BaseController
     public function admin(){
         echo \think\facade\Route::buildUrl();
         halt('amdin');
+    }
+
+
+    public function test()
+    {
+        //Cache::set('name','xd',10);
+       halt(Cache::get('name'));
+       /* try {
+            validate(User::class)->check([
+                'name'  => 'xd',
+                'email' => 'thinkphp@@qq.com',
+            ]);
+
+        } catch (ValidateException $e) {
+            // 验证失败 输出错误信息
+            //dump($e->getError());
+            return json($e->getError());
+        }*/
     }
 }
---
+++ ydshop/app/validate/User.php
@@ -0,0 +1,35 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\validate;
+
+use think\Validate;
+
+class User extends Validate
+{
+    /**
+     * 定义验证规则
+     * 格式：'字段名' =>  ['规则1','规则2'...]
+     *
+     * @var array
+     */
+    protected $rule =   [
+        'name'  => 'require|max:25',
+        'age'   => 'number|between:1,120',
+        'email' => 'email',
+    ];
+
+    /**
+     * 定义错误信息
+     * 格式：'字段名.规则名' =>  '错误信息'
+     *
+     * @var array
+     */
+    protected $message  =   [
+        'name.require' => '名称必须',
+        'name.max'     => '名称最多不能超过25个字符',
+        'age.number'   => '年龄必须是数字',
+        'age.between'  => '年龄只能在1-120之间',
+        'email'        => ['code' => 1005, 'msg' =>'邮箱格式错误'],
+    ];
+}
